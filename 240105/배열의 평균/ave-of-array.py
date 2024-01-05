1a978825cfceaa99574ce014a3e310ac467f60ae
Hoo <kupa10000@naver.com>
2024-01-05 13:43:41 +0900

    Title: 배열의 평균, Time: 141ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240105/배열의 평균/ave-of-array.py
@@ -0,0 +1,23 @@
+arr = [list(map(int, input().split())) for _ in range(2)]
+
+
+for i in range(2):
+    hor_avg = 0
+    hor_avg += sum(arr[i]) / 4
+    print(hor_avg, end=' ')
+print()
+
+for i in range(4):
+    ver_avg = 0
+    for j in range(2):
+        ver_avg += arr[j][i]
+    ver_avg /= 2
+    print(ver_avg, end=' ')
+print()
+
+total = 0
+for a in arr:
+    for n in a:
+        total += n
+tot_avg = total / (len(arr) * len(arr[0]))
+print(tot_avg)
