28968237122a441a40b874451199b3e07f024dfe
Hoo <kupa10000@naver.com>
2024-08-03 21:07:11 +0900

    Title: 최단 Run Length 인코딩, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240803/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,27 @@
+A = list(input())
+N = len(A)
+
+def run_length_encoding(A):
+    count = 1
+    compare = A[0]
+    result = ""
+    for i in range(1, N):
+        if compare == A[i]:
+            count += 1
+        else:
+            result += (compare+str(count))
+            compare = A[i]
+            count = 1
+        if i == 9:
+            result += (A[i]+str(count))
+    return len(result)
+        
+min_length = 10
+for _ in range(N):
+    temp = A[N-1]
+    for i in range(N-1, 0, -1):
+        A[i] = A[i-1]
+    A[0] = temp
+    length = run_length_encoding(A)
+    min_length = min(length, min_length)
+print(min_length)
