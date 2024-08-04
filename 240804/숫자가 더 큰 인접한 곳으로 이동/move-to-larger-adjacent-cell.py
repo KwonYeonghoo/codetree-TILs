1c6f035adebdb50607514696e4f1b5f5e45b5a48
Hoo <kupa10000@naver.com>
2024-08-04 16:45:12 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 106ms, Memory: 25MB, Status: Passed - CodeTree

--- 240804/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240804/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,8 +1,8 @@
 n, r, c = tuple(map(int, input().split()))
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
 def OOB(nr, nc):
     if nr<0 or nr>=n or nc<0 or nc>=n:
@@ -14,18 +14,18 @@ r, c = r-1, c-1
 result = [arr[r][c]]
 
 while True:
-    min_num = 101
-    cur_num = arr[r][c] # 5
+    cur_num = arr[r][c]
     for dir in range(4):
         nr = r + dx[dir]
         nc = c + dy[dir]
         if OOB(nr,nc) or arr[nr][nc] <= cur_num:
             continue
-        min_num = min(min_num, arr[nr][nc])
-        if arr[nr][nc] == min_num:
-            min_r, min_c = nr, nc
-    if min_num == 101:
+        else:
+            cur_num = arr[nr][nc]
+            break
+    if cur_num == arr[r][c]: # 그대로이면 break
         break
-    r, c = min_r, min_c
-    result.append(arr[r][c])
+    else:
+        r,c = nr,nc # 좌표 업데이트
+        result.append(cur_num) # 숫자 추가
 print(' '.join(map(str, result)))
