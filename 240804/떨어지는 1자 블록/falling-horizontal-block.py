79cc48050f9645ea109488b940aa38336e89fe9d
Hoo <kupa10000@naver.com>
2024-08-04 17:02:25 +0900

    Title: 떨어지는 1자 블록, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240804/떨어지는 1자 블록/falling-horizontal-block.py
@@ -0,0 +1,21 @@
+n, m, k = tuple(map(int, input().split()))
+arr = [list(map(int, input().split())) for _ in range(n)]
+k = k-1
+
+def is_fine():
+    flag = True
+    for i in range(n-1):
+        for j in range(k, k+m):
+            arr[i][j] = 1
+            if arr[i+1][j] != 0:
+                flag = False
+        if flag == False:
+            return arr
+        else:
+            for j in range(k, k+m):
+                arr[i][j] = 0
+
+
+new_arr = is_fine()
+for row in new_arr:
+    print(' '.join(map(str, row)))
