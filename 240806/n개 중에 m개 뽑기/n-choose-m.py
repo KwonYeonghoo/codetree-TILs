60a47f3f36e06a4f7dd5bccdb6f88cc64957f7fe
Hoo <kupa10000@naver.com>
2024-08-06 14:32:59 +0900

    Title: n개 중에 m개 뽑기, Time: 113ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240806/n개 중에 m개 뽑기/n-choose-m.py
@@ -0,0 +1,20 @@
+N, M = tuple(map(int, input().split()))
+
+ans = []
+
+def print_ans():
+    print(' '.join(map(str, sorted(ans))))
+
+def permutation(curr_num):
+    if curr_num == M+1:
+        print_ans()
+        return
+
+    for i in range(1, N+1):
+        if curr_num >= 2 and ans[-1] <= i:
+            continue
+        ans.append(i)
+        permutation(curr_num+1)
+        ans.pop()
+
+permutation(1)
